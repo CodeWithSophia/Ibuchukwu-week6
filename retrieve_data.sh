3d12a64f4c405c11134b89472b182a35ed99593d
CodeWithSophia <ezeahibuchukwu@gmail.com>
2024-11-08 23:31:25 +0100

    Set version to 1.0 and updated the retrieve_data file

--- retrieve_data.sh
+++ retrieve_data.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
 # Load version and size from params.yml
-version=$(grep '^version:' params.yml | awk '{print $2}' | tr -d '"')
-size=$(grep "  \"$version\":" params.yml | awk '{print $2}' | tr -d '"')
+version=$(yq '.version' params.yml | sed 's/"//g')
+size=$(yq ".size.\"$version\"" params.yml)
 
 # Define the API endpoint 
-url="https://jsonplaceholder.typicode.com/photos/data?version=$version&size=$size"
+url="https://jsonplaceholder.typicode.com/photos"
 
 # Fetch data from the API using curl and format it with jq
-new_data=$(curl -s "$url" | jq ".[:$size]")
+new_data=$(curl -s "$url" | jq --argjson size "$size" '.[:$size]')
 
 # Check if the datahub/data.json file exists, and compare data if it does
-if [ -f "datahub/data.json" ]; then
-    # Compare the current and newly fetched data
-    current_data=$(jq . datahub/data.json)
-    if [ "$new_data" == "$current_data" ]; then
+if cmp -s <(echo "$new_data") "datahub/data.json"; then
         echo "No changes; data has not changed"
         exit 0
-    fi
+else
+
+	echo "$new_data" > "datahub/data.json"
+	echo "Data has been updated in datahub/data.json"
 fi
-
-# If data is different or datahub/data.json does not exist, save the new data
-echo "$new_data" > datahub/data.json
-echo "Data has been updated in datahub/data.json"
-
